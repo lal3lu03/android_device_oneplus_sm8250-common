5eb88213022d070ea8425d2e65cc5eeab4f9f570
Max Hageneder <max.hageneder@gmail-com>
2026-04-30 14:04:11 +0200

    sm8250-common: Update VENDOR_SECURITY_PATCH to 2026-04-05
    Change-Id: If0cebf59e7783ec75e65efb224f7ef67243d1c8d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -140,7 +140,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security
-VENDOR_SECURITY_PATCH := 2026-02-05
+VENDOR_SECURITY_PATCH := 2026-04-05
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
